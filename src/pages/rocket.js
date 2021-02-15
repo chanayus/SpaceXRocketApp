ebb03211cda523725fe4bf0e574dc0c71b65cdcb
Kittipot <Kittipot321@gmail.com>
2021-02-15 11:27:20 +0700

    Sample Rocket

---
+++ src/pages/rocket.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+const Rocket = () => {
+    const [rockets, setRockets] = useState([])
+    useEffect(
+        () => {
+            const fetchRockets = async () => {
+                const response = await fetch('https://api.spacexdata.com/v3/rockets')
+                const data = await response.json()
+                setRockets(data)
+            }
+            fetchRockets()
+        }, []
+    );
+    return (
+        <ul>
+            {rockets.map((r) => 
+            (
+                <li>{r.rocket_name}</li>
+            ))}
+        </ul>
+    )
+}
+export default Rocket
