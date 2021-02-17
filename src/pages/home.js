60f86e374046d153f0adc2e8ab0a923ecf5b75c4
chanayus <chanayus1526@hotmail.com>
2021-02-17 21:12:41 +0700

    animate scroll

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import logo from '../img/spXlogo.png'
 import styled from 'styled-components'
-import { Link } from "react-router-dom";
+import Aos from "aos";
+import "aos/dist/aos.css"
 import starlinkImg from '../img/starlink-wall.jpg'
 import starlinkLogo from '../img/starlink-logo.png'
 import { Fragment, useEffect, useState } from 'react'
@@ -10,10 +11,12 @@ const Home = () => {
     useEffect(
         () => {
             const fetchLaunches = async () => {
+                
                 const response = await fetch('https://api.spacexdata.com/v3/info')
                 const data = await response.json()
                 setInfo(data)
             }
+            Aos.init({duration: 1200, delay: 120})
             fetchLaunches()
         }, []
     );
@@ -22,7 +25,7 @@ const Home = () => {
             <Navbar />
             <DivContainer className="content-flex" style={{ backgroundImage: `url(${"https://farm9.staticflickr.com/8691/28353012603_ab83b6f5aa_o.jpg"})`, justifyContent: "flex-start" }}>
                 <FlexDiv className="container">
-                    <div style={{width: "80%"}}>
+                    <div data-aos="fade-up" style={{width: "80%"}}>
                         <img src={logo} alt="logo" style={{ maxWidth: "100%" }} />
                         <p style={{ color: "#FFF" }}>{info.summary}</p>
                     </div>
@@ -30,15 +33,15 @@ const Home = () => {
             </DivContainer>
             <DivContainer className="content-flex" style={{ backgroundImage: `url(${starlinkImg})` }}>
                 <FlexDiv className="container">
-                    <div>
+                    <div data-aos="fade-up" data-aos-delay="270">
                         <img src={starlinkLogo} alt="logo" style={{ maxWidth: "100%" }} />
                         <p style={{ color: "#FFF" }}>Starlink is a satellite internet constellation being constructed by SpaceX providing satellite Internet access.</p>
                     </div>
                 </FlexDiv>
             </DivContainer>
             <DivContainer className="content-flex" style={{backgroundImage: `url(${"https://farm2.staticflickr.com/1610/25486858116_9c06dfea59_o.jpg"})`, paddingBottom: "10vmin"}}>
-                <FlexDiv className="container" style={{justifyContent: "space-around", alignItems: "center"}}>
-                    <div style={{width: 500}}>
+                <FlexDiv className="container" style={{justifyContent: "space-around", alignItems: "center"}} >
+                    <div style={{width: 500}} data-aos="fade-right">
                         <h1 style={{fontSize: "7.1vmin"}}>About SpaceX</h1>
                         <ul>
                             <li><b>Founder : </b> {info.founder}</li>
@@ -48,7 +51,7 @@ const Home = () => {
                             <li><b>Headquarters : </b>{info.headquarters.address} {info.headquarters.city} {info.headquarters.state}</li>
                         </ul>
                     </div>   
-                    <div style={{width: 500}}>
+                    <div style={{width: 500}} data-aos="fade-left">
                         <ImgRocket src={"https://farm5.staticflickr.com/4166/34005999880_77684dba4b_o.jpg"} alt="logo"/>  
                     </div>                  
                 </FlexDiv>  
